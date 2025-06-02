438d035a3cdf002a66cd25c8dd40e61f209423f8
Rafael Teixeira Fonseca <rafaelteixeirafonseca@gmail.com>
2025-06-02 03:12:52 -0300

    Mudando o nome para Rafael Teixeira

--- recipes/views.py
+++ recipes/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/home.html', context={
-        'name': 'Luiz Otávio',
+        'name': 'Rafael Teixeira',
         })
